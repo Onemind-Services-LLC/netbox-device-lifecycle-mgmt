8c2081e782a382ed95679cac9af5ae8db9000f8c
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2023-04-22 18:00:42 +0530

    added plugin config

---
+++ netbox_device_lifecycle_mgmt/__init__.py
@@ -0,0 +1,21 @@
+from importlib.metadata import metadata
+
+from extras.plugins import PluginConfig
+
+metadata = metadata('netbox_device_lifecycle_mgmt')
+
+
+class NetBoxDeviceLCMConfig(PluginConfig):
+    name = metadata.get('Name').replace('-', '_')
+    verbose_name = metadata.get('Summary')
+    description = metadata.get('Description')
+    version = metadata.get('Version')
+    author = metadata.get('Author')
+    author_email = metadata.get('Author-email')
+    base_url = 'device-lcm'
+    min_version = '3.5.0'
+    max_version = '3.5.99'
+    required_settings = []
+
+
+config = NetBoxDeviceLCMConfig
