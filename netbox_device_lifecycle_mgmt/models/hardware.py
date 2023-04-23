d2565a2841e48e9f6562ec9d6655331f41efe5f1
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2023-04-23 15:07:08 +0530

    added template content for hardware notices

--- netbox_device_lifecycle_mgmt/models/hardware.py
+++ netbox_device_lifecycle_mgmt/models/hardware.py
@@ -1,5 +1,8 @@
+import datetime
+
 from django.db import models
 from django.urls import reverse
+
 from netbox.models import PrimaryModel
 
 __all__ = ['HardwareNotice']
@@ -68,8 +71,8 @@ class HardwareNotice(PrimaryModel):
             ),
             models.CheckConstraint(
                 check=(
-                    models.Q(inventory_item__isnull=True, device_type__isnull=False)
-                    | models.Q(inventory_item__isnull=False, device_type__isnull=True)
+                        models.Q(inventory_item__isnull=True, device_type__isnull=False)
+                        | models.Q(inventory_item__isnull=False, device_type__isnull=True)
                 ),
                 name='At least one of inventory_item or device_type must be set',
             ),
@@ -90,3 +93,15 @@ class HardwareNotice(PrimaryModel):
 
     def get_absolute_url(self):
         return reverse('plugins:netbox_device_lifecycle_mgmt:hardwarenotice', args=[self.pk])
+
+    @property
+    def expired(self):
+        """
+        Returns True if the notice has expired.
+        """
+        today = datetime.date.today()
+
+        if self.end_of_support_date is not None:
+            return self.end_of_support_date < today
+
+        return self.end_of_sale_date < today
---
+++ netbox_device_lifecycle_mgmt/template_content.py
@@ -0,0 +1,52 @@
+from django.db.models import Q
+
+from extras.plugins import PluginTemplateExtension
+from .models import HardwareNotice
+
+
+class GeneralNoticeExtension(PluginTemplateExtension):
+    def right_page(self):
+        if self.model == 'dcim.devicetype':
+            filters = {'device_type': self.context['object']}
+        elif self.model == 'dcim.inventoryitem':
+            filters = {'inventory_item': self.context['object']}
+        else:
+            filters = {}
+
+        if filters:
+            return self.render(
+                'netbox_device_lifecycle_mgmt/inc/general_notice.html',
+                extra_context={
+                    'object': HardwareNotice.objects.filter(**filters).first()
+                }
+            )
+        return ''
+
+
+class DeviceTypeHardwareNoticeExtension(GeneralNoticeExtension):
+    model = 'dcim.devicetype'
+
+
+class InventoryItemHardwareNoticeExtension(GeneralNoticeExtension):
+    model = 'dcim.inventoryitem'
+
+
+class DeviceHardwareNoticeExtension(PluginTemplateExtension):
+    model = 'dcim.device'
+
+    def left_page(self):
+        obj = self.context['object']
+
+        return self.render(
+            'netbox_device_lifecycle_mgmt/inc/device_notice.html',
+            extra_context={
+                'objects': HardwareNotice.objects.filter(
+                    Q(device_type=obj.device_type) |
+                    Q(inventory_item__in=[i.pk for i in obj.inventoryitems.all()])
+                )
+            }
+        )
+
+
+template_extensions = [DeviceTypeHardwareNoticeExtension, InventoryItemHardwareNoticeExtension,
+                       DeviceHardwareNoticeExtension]
